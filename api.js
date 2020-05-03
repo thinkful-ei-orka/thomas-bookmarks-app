9424554c73ff5211996b486a314ba5bd7bdc49ea
Thomas Davis <ThomasDavisA@gmail.com>
2020-05-03 10:22:05 -0700

    first commit

---
+++ api.js
@@ -0,0 +1,42 @@
+/*
+*
+*   API.js -- Simple JS file to interact with the bookmark API.
+*
+*/
+
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/ThomasDavis';
+
+const getBookmarks = function() {
+  return fetch(`${BASE_URL}/bookmarks`);
+};
+
+const createBookmark = function(bookmark) {
+  let newBookmark = JSON.stringify( { bookmark } );
+  return fetch(`${BASE_URL}/bookmarks`,
+    {method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: newBookmark  });
+};
+
+const updateBookmark = function(id, updateData) {
+  let newData = JSON.stringify(updateData);
+  return fetch(`${BASE_URL}/bookmarks/${id}`,
+    {method: 'PATCH',
+      headers: {'Content-Type': 'application/json'},
+      body: newData
+    });
+};
+
+const deleteBookmark = function(id) {
+  return fetch(`${BASE_URL}/bookmarks/${id}`,
+    {method: 'DELETE',
+      headers: {'Content-Type': 'application/json'}
+    });
+};
+
+export default {
+  getBookmarks,
+  createBookmark,
+  updateBookmark
+};
+
---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+const main = function() {
+    //init listeners here
+};
+
+$(main);
